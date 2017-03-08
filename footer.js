08ee5fd2bfedb832e78ee759503b04385464c721
Monica Rizzolli <monica.rizzolli@gmail.com>
2017-03-08 17:11:16 -0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -5,5 +5,6 @@ document.write('<hr>\
  | <a href="/pessoas/">pessoas</a>\
  | <a href="https://www.meetup.com/arte-e-programacao/"><em>meetups</em></a>\
  | <a href="https://github.com/arteprog">github</a>\
+ | <a href="https://www.patreon.com/arteprog">doações</a>\
  | <a href="https://docs.google.com/forms/d/e/1FAIpQLSdIbkBf-7tUBk6YZ6elDA6hL3VA86fPQNGWshHviAZRcYM3HA/viewform"><b>contato</b></a></p>\
 ');
