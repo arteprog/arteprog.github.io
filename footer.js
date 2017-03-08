48816114242766c2bbdb64eea592288b0cab9f46
Alexandre B A Villares <abav@lugaralgum.com>
2017-03-08 15:18:47 -0300

    Update and rename footer.md to footer.js

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.write('<hr>\
+<p><a href="/">arteprog</a> arte e programaço\
+ | <a href="/projetos/">projetos</a>\
+ | <a href="/cursos/">cursos</a>\
+ | <a href="/pessoas/">pessoas</a>\
+ | <a href="https://www.meetup.com/arte-e-programacao/"><em>meetups</em></a>\
+ | <a href="https://github.com/arteprog">github</a>\
+ | <a href="https://docs.google.com/forms/d/e/1FAIpQLSdIbkBf-7tUBk6YZ6elDA6hL3VA86fPQNGWshHviAZRcYM3HA/viewform"><strong>contato</strong></a></p>\
+');
--- footer.md
+++
@@ -1,8 +0,0 @@
-<hr>
-[arteprog](/) arte e programaço
- | [projetos](/projetos/)
- | [cursos](/cursos/)
- | [pessoas](/pessoas/)
- | [*meetups*](https://www.meetup.com/arte-e-programacao/)
- | [github](https://github.com/arteprog)
- | [**contato**](https://docs.google.com/forms/d/e/1FAIpQLSdIbkBf-7tUBk6YZ6elDA6hL3VA86fPQNGWshHviAZRcYM3HA/viewform)
