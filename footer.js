2813ccaf7c7064b45db74e2f35a38ea11439390d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-01-11 22:40:31 -0300

    reorg

--- footer.js
+++ footer.js
@@ -1,10 +1,10 @@
 document.write('<hr>\
 <p><a href="/"><b>arteprog</b></a> arte e programação\
- | <a href="/projetos/">projetos</a>\
+ | <a href="/projetos/">eventos_passados</a>\
  | <a href="/cursos/">cursos</a>\
  | <a href="/pessoas/">pessoas</a>\
- | <a href="https://www.meetup.com/arte-e-programacao/"><em>meetups</em></a>\
  | <a href="https://github.com/arteprog">github</a>\
  | <a href="https://www.patreon.com/arteprog">apoie</a>\
- | <a href="https://docs.google.com/forms/d/e/1FAIpQLSdIbkBf-7tUBk6YZ6elDA6hL3VA86fPQNGWshHviAZRcYM3HA/viewform"><b>contato</b></a></p>\
+ | <a href="/eventos_passados/">eventos passados"</a>\
+</p>\
 ');
